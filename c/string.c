1a883d214b5a278a6dd4f0db17a4bba3bc23e7ce
jbarawisata <2023barawisata.joseph@fcboe.org>
2022-11-07 11:09:34 -0500

    added some programs

---
+++ c/string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	char name[30];
+	printf("enter the name of your character: ");
+	scanf("%s", &name);
+	printf("your character name is %s\n", name);
+	return 0;
+}
