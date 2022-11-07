2c38432eb80449f044c5db06ca5bf9370a64d82b
jbarawisata <2023barawisata.joseph@fcboe.org>
2022-11-07 11:02:26 -0500

    asfdasfasfdsdfasdksadlkfj

---
+++ c/input.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main() {
+	int var, var2;
+	printf("type in 2 integers: ");
+	scanf("%i%i", &var, &var2);
+	printf("the sum of your numbers is %i\n", var + var2);
+	return 0;
+}
